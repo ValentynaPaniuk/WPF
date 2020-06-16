3bfec2d14ec792ff7c17a33f9153b79e7374215f
Valentyna Paniuk <Valentyna.paniuk@gmail.com>
2020-06-16 15:36:10 +0300

    Homework

---
+++ Examination20200611_WPF/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Examination20200611_WPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new Prop();
+        }
+
+        private void ChoosePage_Click(object sender, RoutedEventArgs e)
+        {
+
+            (DataContext as Prop).PathPage = (e.Source as Button).Tag.ToString();
+        }
+
+        public class Prop : INotifyPropertyChanged
+        {
+            private string path;
+            //prop
+            //propfull
+            public string PathPage
+            {
+                get { return path; }
+                set
+                {
+                    path = value;
+                    OnNotify();
+                }
+            }
+            public event PropertyChangedEventHandler PropertyChanged;
+            private void OnNotify([CallerMemberName]string prop = "")
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+
+    }
+}
